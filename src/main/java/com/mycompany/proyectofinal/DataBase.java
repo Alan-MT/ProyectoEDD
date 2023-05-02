898edc1cd7bae469a7a316136d06172142f4b546
Alan-MT <moralesalan235@gmail.com>
2023-05-02 15:08:06 -0600

    lector xml

--- src/main/java/com/mycompany/proyectofinal/DataBase.java
+++ src/main/java/com/mycompany/proyectofinal/DataBase.java
@@ -9,7 +9,10 @@ import static java.awt.SystemColor.text;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JComponent;
+import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.filechooser.FileNameExtensionFilter;
 
 /**
  *
@@ -19,7 +22,7 @@ public class DataBase {
 
     public principalScreen fra;
     public CargaMasiva carga;
-    public Eliminar borrar ;
+    public Eliminar borrar;
     public IngresoTabla ingreso;
 
     public DataBase() {
@@ -27,21 +30,41 @@ public class DataBase {
         fra.setVisible(true);
         this.carga = new CargaMasiva();
         this.borrar = new Eliminar();
-        this.ingreso  = new IngresoTabla();
-    }
-
-    public void Iniciar() {
-        fra.getCargar().addActionListener(new ActionListener() {
+        this.ingreso = new IngresoTabla();
+        
+        //boton para Cargar el jcomboBox
+                fra.getCargar().addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 if (fra.getComboBoxDatos().getSelectedItem().equals("Carga de Datos")) {
                     showPanel(carga);
                 } else if (fra.getComboBoxDatos().getSelectedItem().equals("Eliminacion")) {
                     showPanel(borrar);
-                } else if (fra.getComboBoxDatos().getSelectedItem().equals("Ingreso de Tablas")){
+                } else if (fra.getComboBoxDatos().getSelectedItem().equals("Ingreso de Tablas")) {
                     showPanel(ingreso);
-                }   
+                }
             }
         });
+                
+                //Boton de abrir el archivo xml
+                 carga.getBotonCargaM().addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                String ruta = "";
+                LectorArchivo lector = new LectorArchivo();
+                JFileChooser archivo = new JFileChooser();
+                FileNameExtensionFilter filtrado = new FileNameExtensionFilter("XML", "xml");
+                archivo.setFileFilter(filtrado);
+                int respuesta = archivo.showOpenDialog(carga.getBotonCargaM());
+                if (respuesta == JFileChooser.APPROVE_OPTION) {
+                    ruta = archivo.getSelectedFile().getPath();
+                }
+                lector.lector(ruta);
+                JOptionPane.showConfirmDialog(carga, ruta);
+            }
+        });
+    }
+
+    public void Iniciar() {
+
     }
 
     public void showPanel(JPanel segundo) {
@@ -51,6 +74,11 @@ public class DataBase {
         fra.getPanelCambiante().add(segundo, BorderLayout.CENTER);
         fra.getPanelCambiante().revalidate();
         fra.getPanelCambiante().repaint();
+    }
+
+    public void CargarDatos() {
+       
+
     }
 
 }
--- src/main/java/com/mycompany/proyectofinal/LectorArchivo.java
+++ src/main/java/com/mycompany/proyectofinal/LectorArchivo.java
@@ -1,18 +1,66 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.proyectofinal;
 
 /**
  *
  * @author maverick
  */
+
+    import java.io.File;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
 public class LectorArchivo {
     
-    public String linea;
+    public LectorArchivo(){}
     
-    public LectorArchivo(){
+    public void lector(String ruta){
+        try {
+      File archivo = new File(ruta);
+      DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+      DocumentBuilder db = dbf.newDocumentBuilder();
+      Document doc = db.parse(archivo);
+
+      doc.getDocumentElement().normalize();
+
+      NodeList listaEstructuras = doc.getElementsByTagName("estructura");
+
+      for (int i = 0; i < listaEstructuras.getLength(); i++) {
+        Node nodoEstructura = listaEstructuras.item(i);
+
+        if (nodoEstructura.getNodeType() == Node.ELEMENT_NODE) {
+          Element elementoEstructura = (Element) nodoEstructura;
+          String nombreTabla = elementoEstructura.getElementsByTagName("tabla").item(0).getTextContent();
+
+          System.out.println("Tabla: " + nombreTabla);
+
+          NodeList listaCampos = elementoEstructura.getChildNodes();
+
+          for (int j = 0; j < listaCampos.getLength(); j++) {
+            Node nodoCampo = listaCampos.item(j);
+
+            if (nodoCampo.getNodeType() == Node.ELEMENT_NODE) {
+              Element elementoCampo = (Element) nodoCampo;
+              String nombreCampo = elementoCampo.getNodeName();
+              String tipoCampo = elementoCampo.getTextContent();
+
+              if ("clave" == nombreCampo) {
+                System.out.print("llave primaria ");
+              }
+              System.out.println("Campo: " + nombreCampo + ", Tipo: " + tipoCampo);
+            }
+          }
+        }
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
     }
     
+    
 }
+
+  
